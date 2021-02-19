9716ff194ce50b7c0c6cc5a0f3c793bafe61f176
UGUR <53657851+ugurkabak35@users.noreply.github.com>
2021-02-19 15:11:42 +0300

    Add project files.

---
+++ LINQSamples/Program.cs
@@ -0,0 +1,43 @@
+﻿using LINQSamples.Data;
+using System;
+using System.Linq;
+
+namespace LINQSamples
+{
+    class ProductModel
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContext())
+            {
+                //var products = db.Products.ToList();
+                //var products = db.Products.Select(p => p.ProductName).ToList();
+                //var products = db.Products.Select(p => new { p.ProductName,p.UnitPrice }).ToList();
+                //var products = db.Products.Select(p => 
+                //    new ProductModel 
+                //    { 
+                //        Name = p.ProductName,
+                //        Price = p.UnitPrice 
+                //    }).ToList();
+
+                //var product = db.Products.First();
+                var product = db.Products.Select(p=>new { p.ProductName, p.UnitPrice }).FirstOrDefault();
+
+                Console.WriteLine(product.ProductName + ' ' + product.UnitPrice);
+
+                //foreach (var p in products)
+                //{
+                //    Console.WriteLine(p.Name + ' ' + p.Price);
+                //}
+            }
+
+            Console.ReadLine();
+        }
+      
+    }
+}
